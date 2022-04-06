89c996807dc033b824df4e38c155f7adfb9163ba
BEATRIZ PIFFER SILVA <81167411+Beatrizpiffer@users.noreply.github.com>
2022-04-06 14:56:19 -0300

    Create getElementsByClassName.js

---
+++ getElementsByClassName.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Olá mundo</title>
+</head>
+<body>
+
+    <div class="carro">Gol</div>
+    <div class="carro">Camaro</div>
+
+     <script scr="js\app.js"> </script>
+</body>
+</html>
+
+console.log(
+    document.getElementsByClassName("carro")
+)
